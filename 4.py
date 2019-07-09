f9c6377dd078f0b13a730c75f3345adee66c97bf
db2398 <51949608+db2398@users.noreply.github.com>
2019-07-09 15:04:17 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,11 +1,11 @@
-days,gone=map(str,input().split())
-wave=0
-if len(days)>len(gone):
-  days,gone=gone,mah
+vs,vit1=map(str,input().split())
+was=0
+if len(vs)>len(vit1):
+  vs,vit1=vit1,dbj
 i=0
-while i<len(days):
-  silent+=(ord(gone[i])-ord(days[i]))
+while i<len(vs):
+  was+=(ord(vit1[i])-ord(vs[i]))
   i+=1
-for i in range(i,len(gone)):
-  silent+=ord(gone[i])-ord('a')+1
-print(silent)
+for i in range(i,len(vit1)):
+  was+=ord(vit1[i])-ord('a')+1
+print(was)
